fb0e765db0b9e0d7421fd3a1dd9dfd6bcb19b836
Cat Tuong <88124873+blackcoffee19@users.noreply.github.com>
2024-05-24 09:51:22 +0700

    24/5 commit

--- src/auth/APIService.js
+++ src/auth/APIService.js
@@ -1,18 +1,5 @@
 class APIService {    
     URL_REST_API = 'http://192.168.1.5:9001/api';
-    // getFriendPosts(){
-    //     let url = URL_REST_API+"friendposts/1";
-    //     console.log(url);
-    //     return fetch(url,{ 
-    //         method: 'get',
-    //             headers: {
-    //             'Accept': 'application/json, text/plain, */*',
-    //             'Content-Type': 'application/json',
-    //             },
-    //             'credentials': 'same-origin'
-    //     })
-    //     .then(res => res.json());        
-    // }
 
 }
 
